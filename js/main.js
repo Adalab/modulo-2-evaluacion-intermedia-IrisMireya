4b77ba369608e73d88c85803a45ad1a36fff56d4
Iris Mireya <irismzb_86@hotmail.com>
2022-03-29 10:26:19 +0200

    options added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const number = document.querySelector('.js-InputNumber');
-const select = document.querySelector('.js-inputselect');
-const btn = document.querySelector('.js-btnn');
+const select = document.querySelector('.js-select');
+const number = document.querySelector('.js-inputNumber');
+const btn = document.querySelector('.js-btn');
+const sum = document.querySelector('.js-inputSum');
 
 
-.addEventListener('submit', (event) => {
+btn.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log ('holiss');
 });
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+
+  }
+
+  function playGame(){
+      if(randomNumber ===)
+  }
