6e62f08fb1de69c20f286fc85e682579fd7a0c1a
Iris Mireya <irismzb_86@hotmail.com>
2022-03-29 10:35:18 +0200

    if & else

--- js/main.js
+++ js/main.js
@@ -6,16 +6,20 @@ const btn = document.querySelector('.js-btn');
 const sum = document.querySelector('.js-inputSum');
 
 
-btn.addEventListener('submit', (event) => {
+btn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log ('holiss');
 });
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-
-  }
-
-  function playGame(){
-      if(randomNumber ===)
+    
+    if(numAleact % 2 === 0) {
+        mainElement.classList.add('Has ganado el doble de lo apostado  ');
+        mainElement.classList.remove('Has perdido lo apostado');
+    }
+    else{
+        mainElement.classList.add('Has perdido lo apostado');
+        mainElement.classList.remove('Has ganado el doble de lo apostado ');
+    }
   }
