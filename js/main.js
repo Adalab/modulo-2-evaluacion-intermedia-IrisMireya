78df753044a8daaff5fbbb7bb4864e5ea68d0f59
Iris Mireya <irismzb_86@hotmail.com>
2022-03-29 11:19:22 +0200

    numAleat

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const select = document.querySelector('.js-select');
-const number = document.querySelector('.js-inputNumber');
+const text = document.querySelector('.js-inputText');
 const btn = document.querySelector('.js-btn');
 const sum = document.querySelector('.js-inputSum');
 
@@ -9,18 +9,22 @@ const sum = document.querySelector('.js-inputSum');
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log ('¡¡Ha hecho click!!');
+    checkNum()
 });
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-    const numAleact = document.querySelector('.js-play');
-
-    if(numAleact % 2 === 0) {
-        numAleact.classList.add('¡¡¡Has ganado el doble de lo apostado!!!');
-        numAleact.classList.remove('¡Has perdido lo apostado!');
+}
+    function checkNum() {
+    const num = parseInt(select.value);
+    const numAleat =getRandomNumber(6) ;
+    console.log("Input number: " + num);
+    console.log("Num comp: " + numAleat);
+   
+    if(num === numAleat) {
+        text.innerHTML = '¡¡¡Has ganado el doble de lo apostado!!!';
     }
     else{
-        numAleact.classList.add('¡Has perdido lo apostado!');
-        numAleact.classList.remove('¡¡¡Has ganado el doble de lo apostado!!!');
+        text.innerHTML = '¡Has perdido lo apostado!';
     }
-  }
+}
