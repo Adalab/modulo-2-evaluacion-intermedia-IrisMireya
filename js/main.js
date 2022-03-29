6911b56cbff0428cfae735c8e572abafb04f5491
Iris Mireya <irismzb_86@hotmail.com>
2022-03-29 10:07:14 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-'use strict'
+'use strict';
+
+const number = document.querySelector('.js-InputNumber');
+const select = document.querySelector('.js-inputselect');
+const btn = document.querySelector('.js-btnn');
+
+
+.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log ('holiss');
+});
