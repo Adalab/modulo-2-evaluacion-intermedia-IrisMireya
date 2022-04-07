c206b5b89e242c2bccb4937c4815197aee91a216
Iris Mireya <irismzb_86@hotmail.com>
2022-04-07 11:36:11 +0200

    money amountUser btnreset

--- js/main.js
+++ js/main.js
@@ -3,28 +3,39 @@
 const select = document.querySelector('.js-select');
 const text = document.querySelector('.js-inputText');
 const btn = document.querySelector('.js-btn');
-const sum = document.querySelector('.js-inputSum');
+const btnreset = document.querySelector('.js-reset');
+const amount = document.querySelector('.js-amount');
+const blc = document.querySelector('.js-balance');
+let money = parseInt(blc.innerText);
+let amountUser = parseInt(amount.innerHTML);
+const max = 6;
+const randomNumber = getRandomNumber(max);
+console.log(randomNumber);
 
 
-btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log ('¡¡Ha hecho click!!');
-    checkNum()
-});
-
+    
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-    function checkNum() {
+btn.addEventListener('click', handleClickBtn);
+btnreset.addEventListener('click', resetGame);
+function handleClickBtn(event){
+    event.preventDefault();
+    checkNum(money, amountUser);
+}
+function checkNum() {
     const num = parseInt(select.value);
     const numAleat = getRandomNumber(6) ;
-    console.log("Input number: " + num);
-    console.log("Num comp: " + numAleat);
    
     if(num === numAleat) {
-        text.innerHTML = '¡¡¡Has ganado el doble de lo apostado!!!';
+        text.innerHTML = '¡¡¡Has ganado el doble de lo apostado!!! &#128526';
+        money = money + amountUser *2;
+        amount.innerHTML = money;
     }
     else{
-        text.innerHTML = '¡Has perdido lo apostado!';
+        text.innerHTML = '¡Has perdido lo apostado! &#128557';
+        money = money - amountUser;
+        amount.innerHTML = money;
     }
 }
+function resetGame();
